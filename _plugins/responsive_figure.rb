90aa5e23b8d96bf13ede0427cc7144fe6b532ffb
yuxqiu <yuxqiu@proton.me>
2025-02-26 17:43:58 +0000

    fix: only match certain image format

--- _plugins/responsive_figure.rb
+++ _plugins/responsive_figure.rb
@@ -1,10 +1,11 @@
 Jekyll::Hooks.register [:posts, :pages], :pre_render do |doc|
-    # (?m) ensures that ^ matches the start of each line.
-    # ^(?!\s) asserts that the line does not start with any whitespace.
-    doc.content = doc.content.gsub(/(?m)^(?!\s)!\[([^\]]*)\]\(([^)]+)\)/) do
+    doc.content = doc.content.gsub(/(?mi)^(?!\s)!\[([^\]]*)\]\(([^)]+\.(?:jpg|jpeg|png|tiff|gif))\)(?:\r?\n)/) do
       alt_text = Regexp.last_match(1)
       src      = Regexp.last_match(2)
 
-      "{% include figure.liquid alt=\"#{alt_text}\" path=\"#{src}\" %}"
+      replacement = "{% include figure.liquid alt=\"#{alt_text}\" path=\"#{src}\" %}"
+
+      # Always append a newline.
+      replacement + "\n"
     end
   end
