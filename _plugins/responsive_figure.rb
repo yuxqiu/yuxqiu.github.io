2eab01a93c354e5d250cc2013e36e7ac7d0c1dd5
yuxqiu <yuxqiu@proton.me>
2025-02-27 20:02:36 +0000

    feat: add caption support

--- _plugins/responsive_figure.rb
+++ _plugins/responsive_figure.rb
@@ -1,12 +1,15 @@
 Jekyll::Hooks.register [:posts, :pages], :pre_render do |doc|
-    doc.content = doc.content.gsub(/(?mi)^(\s*)!\[([^\]]*)\]\(([^)]+\.(?:jpg|jpeg|png|tiff|gif))\)(?:\r?\n)?/) do
+    # match ![alt](/assets/src)
+    #       *caption*
+    #
+    # This content will eagerly match any caption under the image, even if their indent does not match.
+    # It works well as long as any non-caption content is not directly put under the image and starts with *.
+    doc.content = doc.content.gsub(/(?mi)^(\s*)!\[([^\]]*)\]\(\/assets\/([^)]+\.(?:jpg|jpeg|png|tiff|gif))\)(?:\r?\n\s*(?:\*([^\*]+)\*))?/) do
       indent   = Regexp.last_match(1)
       alt_text = Regexp.last_match(2)
       src      = Regexp.last_match(3)
+      caption  = Regexp.last_match(4)
 
-      replacement = "{% include figure.liquid alt=\"#{alt_text}\" path=\"#{src}\" indent=\"#{indent}\" %}"
-
-      # Ensure a newline is always added after the replacement
-      replacement + "\n"
+      replacement = "{% include figure.liquid alt=\"#{alt_text}\" path=\"/assets/#{src}\" indent=\"#{indent}\" caption=\"#{caption}\" %}"
     end
   end
