82073346ce30ce2c559327afc4c22895bdc14b60
yuxqiu <yuxqiu@proton.me>
2025-02-27 10:15:19 +0000

    feat: support auto conversion from indented img to responsive img

--- _plugins/responsive_figure.rb
+++ _plugins/responsive_figure.rb
@@ -1,11 +1,12 @@
 Jekyll::Hooks.register [:posts, :pages], :pre_render do |doc|
-    doc.content = doc.content.gsub(/(?mi)^(?!\s)!\[([^\]]*)\]\(([^)]+\.(?:jpg|jpeg|png|tiff|gif))\)(?:\r?\n)/) do
-      alt_text = Regexp.last_match(1)
-      src      = Regexp.last_match(2)
+    doc.content = doc.content.gsub(/(?mi)^(\s*)!\[([^\]]*)\]\(([^)]+\.(?:jpg|jpeg|png|tiff|gif))\)(?:\r?\n)?/) do
+      indent   = Regexp.last_match(1)
+      alt_text = Regexp.last_match(2)
+      src      = Regexp.last_match(3)
 
-      replacement = "{% include figure.liquid alt=\"#{alt_text}\" path=\"#{src}\" %}"
+      replacement = "{% include figure.liquid alt=\"#{alt_text}\" path=\"#{src}\" indent=\"#{indent}\" %}"
 
-      # Always append a newline.
+      # Ensure a newline is always added after the replacement
       replacement + "\n"
     end
   end
